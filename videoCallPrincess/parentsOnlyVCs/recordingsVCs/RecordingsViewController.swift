954b120610a11400b4552161920207d67f48991c
Omar Yusuf <omaryusuf@Sumayas-MacBook-Air.local>
2018-07-16 00:01:26 -0400

    second commit

--- videoCallPrincess/parentsOnlyVCs/recordingsVCs/RecordingsViewController.swift
+++ videoCallPrincess/parentsOnlyVCs/recordingsVCs/RecordingsViewController.swift
@@ -11,174 +11,230 @@ import AVKit
 import AVFoundation
 
 protocol VideoTableViewCellDelegate : class {
-
-  func shareVideo(senderCell: VideoCellTableViewCell, shareVideoURL: URL, silentVideoName: String)
-  func deleteVideo(senderCell: VideoCellTableViewCell, deleteVideoURL: URL)
+    
+    func shareVideo(senderCell: VideoCellTableViewCell, shareVideoURL: URL, silentVideoName: String)
+    func deleteVideo(senderCell: VideoCellTableViewCell, deleteVideoURL: URL)
 }
 
 class RecordingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, VideoTableViewCellDelegate{
-  
-  
-  func shareVideo(senderCell: VideoCellTableViewCell, shareVideoURL: URL, silentVideoName: String) {
-    
-    shareVideoURLFinal = shareVideoURL
-    silentVideoNameFinal = silentVideoName
     
     
-    let shareAlert = self.storyboard?.instantiateViewController(withIdentifier: "shareAlertAlertID") as! ShareViewController
-    shareAlert.providesPresentationContextTransitionStyle = true
-    shareAlert.definesPresentationContext = true
-    shareAlert.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
-    shareAlert.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
-    shareAlert.delegate = self
-    self.present(shareAlert, animated: true, completion: nil)
-    
-    
-    }
-
-  func deleteVideo(senderCell: VideoCellTableViewCell, deleteVideoURL: URL) {
-  
-    let alert = UIAlertController(title: "Alert", message: "Would you like to delete this video?", preferredStyle: UIAlertControllerStyle.alert)
-
-    alert.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.destructive, handler: { action in
-      
-      let fileManager = FileManager.default
-      do {
-        try fileManager.removeItem(at: deleteVideoURL)
-      }
-      catch let error as NSError {
-        print("Ooops! Something went wrong: \(error)")
-      }
-      
-      if let indexPath = self.tableView.indexPath(for: senderCell) {
-        self.urls.remove(at: indexPath.row)
-        self.tableView.beginUpdates()
-        self.tableView.deleteRows(at: [indexPath], with: .automatic)
-        self.tableView.endUpdates()
+    func shareVideo(senderCell: VideoCellTableViewCell, shareVideoURL: URL, silentVideoName: String) {
         
-        if self.urls.count == 0 {
-          self.noRecordingsView.isHidden = false
-        } else {
-          self.noRecordingsView.isHidden = true
-      }
-      }
-      
-    
-    }))
-    alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
-    self.present(alert, animated: true, completion: nil)
-
-    
+        shareVideoURLFinal = shareVideoURL
+        silentVideoNameFinal = silentVideoName
+        
+        
+        let shareAlert = self.storyboard?.instantiateViewController(withIdentifier: "shareAlertAlertID") as! ShareViewController
+        shareAlert.providesPresentationContextTransitionStyle = true
+        shareAlert.definesPresentationContext = true
+        shareAlert.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
+        shareAlert.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        shareAlert.delegate = self
+        self.present(shareAlert, animated: true, completion: nil)
+        
+        
     }
-
-  @IBOutlet weak var tableView: UITableView!
-  @IBOutlet weak var noRecordingsView: UIView!
-  
-  var urls: [URL] = []
-  var shareVideoURLFinal: URL!
-  var silentVideoNameFinal: String!
-  var verticalSelected = true
-  var isDone = false
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    NotificationCenter.default.addObserver(self, selector: #selector(self.loadList(_:)), name: NSNotification.Name(rawValue: "load"), object: nil)
     
-    let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-
-    do {
-      let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: [.contentModificationDateKey], options: [.skipsHiddenFiles])
-
-      let movFiles = directoryContents.filter{ $0.pathExtension == "mov" }
-      print("mov urls:", movFiles)
-
-
-      let temp = directoryContents.map { url in
-        (url, (try? url.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast)
+    func deleteVideo(senderCell: VideoCellTableViewCell, deleteVideoURL: URL) {
+        
+        let alert = UIAlertController(title: "Alert", message: "Would you like to delete this video?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.destructive, handler: { action in
+            
+            let fileManager = FileManager.default
+            do {
+                try fileManager.removeItem(at: deleteVideoURL)
+            }
+            catch let error as NSError {
+                print("Ooops! Something went wrong: \(error)")
+            }
+            
+            if let indexPath = self.tableView.indexPath(for: senderCell) {
+                self.urls.remove(at: indexPath.row)
+                self.tableView.beginUpdates()
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+                self.tableView.endUpdates()
+                
+                if self.urls.count == 0 {
+                    self.noRecordingsView.isHidden = false
+                } else {
+                    self.noRecordingsView.isHidden = true
+                }
+            }
+            
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noRecordingsView: UIView!
+    
+    fileprivate var tasks = [URLSessionTask]()
+    
+    var urls: [URL] = []
+    var cells: [Int: VideoCellTableViewCell] = [:]
+    var shareVideoURLFinal: URL!
+    var silentVideoNameFinal: String!
+    var verticalSelected = true
+    var isDone = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.prefetchDataSource = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.loadList(_:)), name: NSNotification.Name(rawValue: "load"), object: nil)
+        
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: [.contentModificationDateKey], options: [.skipsHiddenFiles])
+            
+            let movFiles = directoryContents.filter{ $0.pathExtension == "mov" }
+            print("mov urls:", movFiles)
+            
+            
+            let temp = directoryContents.map { url in
+                (url, (try? url.resourceValues(forKeys: [.contentModificationDateKey]))?.contentModificationDate ?? Date.distantPast)
+                }
+                .sorted(by: { $0.1 > $1.1 }) // sort descending modification dates
+                .map { $0.0 } // extract file names
+            
+            self.urls = temp
+            self.tableView.reloadData()
+            
+            if movFiles.isEmpty == true {
+                noRecordingsView.isHidden = false
+            } else {
+                noRecordingsView.isHidden = true
+            }
+            
+        } catch {
+            print(error.localizedDescription)
         }
-        .sorted(by: { $0.1 > $1.1 }) // sort descending modification dates
-        .map { $0.0 } // extract file names
-
-      self.urls = temp
-      self.tableView.reloadData()
-      
-      if movFiles.isEmpty == true {
-        noRecordingsView.isHidden = false
-      } else {
-        noRecordingsView.isHidden = true
-      }
-      
-    } catch {
-      print(error.localizedDescription)
-    }
-  
-  }
-  
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    
-    let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask,true)[0] as NSString
-    let destinationPath = documentsPath.appendingPathComponent("overlapVideo.mov")
-    let fileManager = FileManager.default
-    
-    if fileManager.fileExists(atPath: destinationPath) {
-      do {
-        try fileManager.removeItem(atPath: destinationPath)
-        print("Deleted Overlap Video")
-      }
-      catch let error as NSError {
-        print("Ooops! Something went wrong: \(error)")
-      }
-    } else {
-      print("Overlap file does not exist")
-    }
-  }
-  
-  @objc func loadList(_ notification: NSNotification){
-    //load data here
-      viewDidLoad()
-      print("notification received")
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.urls.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! VideoCellTableViewCell
-    cell.url = self.urls[indexPath.row]
-    cell.delegate = self
-    cell.setupVideo()
-//    cell.setupSilentVideo()
-    
-    
-    DispatchQueue.global(qos: .userInteractive).async {
-      let player = AVPlayer(url: cell.url)
-      
-      cell.extractSilentVideo()
-      let silenturl = Bundle.main.path(forResource: cell.silentVideo, ofType: "mp4")
-      let silentPlayer = AVPlayer(url: URL(fileURLWithPath: "\(silenturl ?? "nil")"))
-      
-      DispatchQueue.main.async {
-        cell.videoView?.playerLayer.player = player
-        cell.silentVideoView?.playerLayer.player = silentPlayer
-      }
-      
+        
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask,true)[0] as NSString
+        let destinationPath = documentsPath.appendingPathComponent("overlapVideo.mov")
+        let fileManager = FileManager.default
+        
+        if fileManager.fileExists(atPath: destinationPath) {
+            do {
+                try fileManager.removeItem(atPath: destinationPath)
+                print("Deleted Overlap Video")
+            }
+            catch let error as NSError {
+                print("Ooops! Something went wrong: \(error)")
+            }
+        } else {
+            print("Overlap file does not exist")
+        }
+    }
     
-    return cell
-  }
+    @objc func loadList(_ notification: NSNotification){
+        //load data here
+        viewDidLoad()
+        print("notification received")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.urls.count
+    }
+    
+    fileprivate func cancelPrefetchVideo(forItemAtIndex index: Int) {
+        let url = self.urls[index]
+        guard let taskIndex = tasks.index(where: { $0.originalRequest?.url == url }) else {
+            return
+        }
+        
+        self.cells[index] = nil
+        let task = tasks[taskIndex]
+        task.cancel()
+        tasks.remove(at: taskIndex)
+    }
 
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 479
-  }
-  
+    
+    fileprivate func prefetchVideo(forItemAtIndex index: Int) {
+        let url = self.urls[index]
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell") as! VideoCellTableViewCell
+        
+        
+        guard tasks.index(where: { $0.originalRequest?.url == url }) == nil else {
+            // We're already downloading the image.
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                cell.url = url
+                cell.delegate = self
+                cell.setupVideo()
+                
+                DispatchQueue.global(qos: .userInteractive).async {
+                    let player = AVPlayer(url: cell.url)
+                    let silentURL: NSURL = Bundle.main.url(forResource: "princessOneEpisodeOne", withExtension: "mp4")! as NSURL
+                    let silentPlayer = AVPlayer(url: silentURL as URL)
+                    let silentLayer = AVPlayerLayer(player: silentPlayer)
+                   
+                    
+                    DispatchQueue.main.async {
+                        cell.videoView?.playerLayer.player = player
+                        cell.silentVideoView?.playerLayer.player = silentLayer.player
+                    }
+                }
+                    
+                self.cells[index] = cell
+                
+                let indexPath = IndexPath(row: index, section: 0)
+                if self.tableView.indexPathsForVisibleRows?.contains(indexPath) ?? false {
+                    self.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .fade)
+                    
+                }
+            }
+        }
+        
+        task.resume()
+        tasks.append(task)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell") as! VideoCellTableViewCell
+        
+        if self.cells.index(forKey: indexPath.row) == nil{
+            self.prefetchVideo(forItemAtIndex: indexPath.row)
+            
+        }
+        else{
+            cell = self.cells[indexPath.row]!
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 479
+    }
+}
+
+// MARK: - UITableViewDataSourcePrefetching
+extension RecordingsViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { self.prefetchVideo(forItemAtIndex: $0.row) }
+    }
+    
+    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        //indexPaths.forEach { self.cancelPrefetchVideo(forItemAtIndex: $0.row) }
+    }
 }
 
 extension RecordingsViewController: ShareViewDelegate {
-    
     func cancelBtnAction() {
         verticalSelected = true
     }
@@ -190,42 +246,33 @@ extension RecordingsViewController: ShareViewDelegate {
     func boxBtnAction() {
         verticalSelected = false
     }
-  
-  func shareBtnAction() {
-    let silentPath = Bundle.main.path(forResource: silentVideoNameFinal, ofType: "mp4")!
-    let silentUrl = URL.init(fileURLWithPath: silentPath)
     
-    print("\(silentVideoNameFinal)")
-    
-    
-    let exporter = AVAssetExportSession.overlapVideos(shareVideoURLFinal,
-                                                      secondUrl: silentUrl,
-                                                      isPortraitMode: verticalSelected,
-                                                                        exportedFilename: "overlapVideo.mov")
-
-    
-    exporter?.exportAsynchronously {
-
-      DispatchQueue.main.async {
+    func shareBtnAction() {
+        let silentPath = Bundle.main.path(forResource: silentVideoNameFinal, ofType: "mp4")!
+        let silentUrl = URL.init(fileURLWithPath: silentPath)
         
-        let activityVC = UIActivityViewController(activityItems: [exporter?.outputURL ?? self.shareVideoURLFinal], applicationActivities: nil)
+        print("\(silentVideoNameFinal)")
 
-        activityVC.popoverPresentationController?.sourceView = self.view
-
-        self.present(activityVC, animated: true, completion: nil)
+        let exporter = AVAssetExportSession.overlapVideos(shareVideoURLFinal,
+                                                          secondUrl: silentUrl,
+                                                          isPortraitMode: verticalSelected,
+                                                          exportedFilename: "overlapVideo.mov")
         
-        self.isDone = true
+        exporter?.exportAsynchronously {
+            DispatchQueue.main.async {
+                
+                let activityVC = UIActivityViewController(activityItems: [exporter?.outputURL ?? self.shareVideoURLFinal], applicationActivities: nil)
+                
+                activityVC.popoverPresentationController?.sourceView = self.view
+                self.present(activityVC, animated: true, completion: nil)
+                self.isDone = true
+            }
+        }
         
-        
+        if isDone == true {
+            isDone = false
+            verticalSelected = true
+            
         }
     }
-    
-    if isDone == true {
-        
-      isDone = false
-      verticalSelected = true
-      
-    }
-
-  }
 }
--- videoCallPrincess/parentsOnlyVCs/recordingsVCs/VideoCellTableViewCell.swift
+++ videoCallPrincess/parentsOnlyVCs/recordingsVCs/VideoCellTableViewCell.swift
@@ -11,61 +11,61 @@ import AVFoundation
 
 class VideoCellTableViewCell: UITableViewCell {
     
-  weak var delegate: VideoTableViewCellDelegate?
-
-  @IBOutlet weak var videoView: VideoView!
-  
-
-  @IBOutlet weak var silentVideoView: VideoView!
-
-
-  @IBOutlet weak var playBtnUI: UIButton!
-  @IBOutlet weak var stopBtnUI: UIButton!
-  @IBOutlet weak var shareBtnUI: UIButton!
-  @IBOutlet weak var fullScreenBtn: UIButton!
-  @IBOutlet weak var progressView: UIProgressView!
-  @IBOutlet weak var leftClock: UILabel!
-  @IBOutlet weak var rightClock: UILabel!
+    weak var delegate: VideoTableViewCellDelegate?
     
-  var url: URL!
-  var videoURL: URL!
-  var deleteURL: URL!
-  var silentVideo: String!
-  var timer = Timer()
-  var progressTimer = Timer()
-  
+    @IBOutlet weak var videoView: VideoView!
+    
+    
+    @IBOutlet weak var silentVideoView: VideoView!
+    
+    
+    @IBOutlet weak var playBtnUI: UIButton!
+    @IBOutlet weak var stopBtnUI: UIButton!
+    @IBOutlet weak var shareBtnUI: UIButton!
+    @IBOutlet weak var fullScreenBtn: UIButton!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var leftClock: UILabel!
+    @IBOutlet weak var rightClock: UILabel!
+    
+    var url: URL!
+    var videoURL: URL!
+    var deleteURL: URL!
+    var silentVideo: String!
+    var timer = Timer()
+    var progressTimer = Timer()
+    
     var upSeconds = 0
     var upTimer = Timer()
     var upTimerIsOn = false
     
-  var silentPlayerTimer = Timer()
-
-  var durationTime: Float64!
-  var durationSeconds: Int!
-  var durationMinutes: Int!
-
-  override func awakeFromNib() {
-    super.awakeFromNib()
-
-    playBtnUI.layer.cornerRadius = playBtnUI.frame.size.width/2
-    stopBtnUI.layer.cornerRadius = stopBtnUI.frame.size.width/2
+    var silentPlayerTimer = Timer()
     
-  }
+    var durationTime: Float64!
+    var durationSeconds: Int!
+    var durationMinutes: Int!
     
-  func setupVideo() {
-    let asset = AVAsset(url: url)
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        playBtnUI.layer.cornerRadius = playBtnUI.frame.size.width/2
+        stopBtnUI.layer.cornerRadius = stopBtnUI.frame.size.width/2
+        
+    }
     
-//    let player = AVPlayer(url: url)
-//
-//    videoView?.playerLayer.player = player
-  
-    
-    let duration = asset.duration
-    durationTime = CMTimeGetSeconds(duration)
-    durationMinutes = Int((durationTime.truncatingRemainder(dividingBy: 3600)) / 60)
-    durationSeconds = Int(durationTime.truncatingRemainder(dividingBy: 60))
-    rightClock.text = String(format: "%02i:%02i", arguments: [durationMinutes,durationSeconds])
-  }
+    func setupVideo() {
+        let asset = AVAsset(url: url)
+        
+        //    let player = AVPlayer(url: url)
+        //
+        //    videoView?.playerLayer.player = player
+        
+        
+        let duration = asset.duration
+        durationTime = CMTimeGetSeconds(duration)
+        durationMinutes = Int((durationTime.truncatingRemainder(dividingBy: 3600)) / 60)
+        durationSeconds = Int(durationTime.truncatingRemainder(dividingBy: 60))
+        rightClock.text = String(format: "%02i:%02i", arguments: [durationMinutes,durationSeconds])
+    }
     
     func setupSilentVideo() {
         
@@ -75,11 +75,11 @@ class VideoCellTableViewCell: UITableViewCell {
             debugPrint("Video not found")
             return
         }
-      
-      let silentPlayer = AVPlayer(url: URL(fileURLWithPath: url))
-      
-      silentVideoView?.playerLayer.player = silentPlayer
-      
+        
+        let silentPlayer = AVPlayer(url: URL(fileURLWithPath: url))
+        
+        silentVideoView?.playerLayer.player = silentPlayer
+        
     }
     
     func extractSilentVideo() {
@@ -91,65 +91,65 @@ class VideoCellTableViewCell: UITableViewCell {
         let newIndex = myString.index(myString.startIndex, offsetBy: 21)
         let myNewSubstring = myString[..<newIndex] // Hello
         silentVideo = String(myNewSubstring)
-        print(silentVideo)
+        print("....", silentVideo)
     }
     
-  func playVideo() {
-    let seconds : Int64 = 0
-    let preferredTimeScale : Int32 = 1
-    let seekTime : CMTime = CMTimeMake(seconds, preferredTimeScale)
-    
-    videoView.player?.seek(to: seekTime)
-    videoView.player?.play()
-    
-    silentVideoView.player?.seek(to: seekTime)
-    silentVideoView.player?.isMuted = true
-    silentVideoView.player?.play()
-    
-    
-  }
-    
-
-  @IBAction func startBtn(_ sender: Any) {
-    playVideo()
-    
-    timer = Timer.scheduledTimer(timeInterval: durationTime, target: self, selector: #selector(VideoCellTableViewCell.silentVideoPause), userInfo: nil, repeats: true)
-    
-    playBtnUI.isHidden = true
-    stopBtnUI.isHidden = false
-    fullScreenBtn.isHidden = false
-    progressView.isHidden = false
-    leftClock.isHidden = false
-    rightClock.isHidden = false
-    
-    progressTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(VideoCellTableViewCell.go), userInfo: nil, repeats: true)
-    RunLoop.main.add(progressTimer, forMode: RunLoopMode.commonModes)
-    
-    if upTimerIsOn == false {
-        upTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-        RunLoop.main.add(upTimer, forMode: RunLoopMode.commonModes)
+    func playVideo() {
+        let seconds : Int64 = 0
+        let preferredTimeScale : Int32 = 1
+        let seekTime : CMTime = CMTimeMake(seconds, preferredTimeScale)
+        
+        videoView.player?.seek(to: seekTime)
+        videoView.player?.play()
+        
+        silentVideoView.player?.seek(to: seekTime)
+        silentVideoView.player?.isMuted = true
+        silentVideoView.player?.play()
+        
+        
     }
-  }
-
-  @IBAction func stopBtn(_ sender: Any) {
-    videoView.player?.pause()
-    silentVideoView.player?.pause()
-    timer.invalidate()
-    playBtnUI.isHidden = false
-    stopBtnUI.isHidden = true
-    fullScreenBtn.isHidden = true
-    progressView.isHidden = true
-    leftClock.isHidden = true
-    rightClock.isHidden = true
-    progressTimer.invalidate()
-    progressView.progress = 0.00
     
-    upTimer.invalidate()
-    upSeconds = 0
-    leftClock.text = "\(timeFormatted(upSeconds))"
-    upTimerIsOn = false
-
-  }
+    
+    @IBAction func startBtn(_ sender: Any) {
+        playVideo()
+        
+        timer = Timer.scheduledTimer(timeInterval: durationTime, target: self, selector: #selector(VideoCellTableViewCell.silentVideoPause), userInfo: nil, repeats: true)
+        
+        playBtnUI.isHidden = true
+        stopBtnUI.isHidden = false
+        fullScreenBtn.isHidden = false
+        progressView.isHidden = false
+        leftClock.isHidden = false
+        rightClock.isHidden = false
+        
+        progressTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(VideoCellTableViewCell.go), userInfo: nil, repeats: true)
+        RunLoop.main.add(progressTimer, forMode: RunLoopMode.commonModes)
+        
+        if upTimerIsOn == false {
+            upTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            RunLoop.main.add(upTimer, forMode: RunLoopMode.commonModes)
+        }
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        videoView.player?.pause()
+        silentVideoView.player?.pause()
+        timer.invalidate()
+        playBtnUI.isHidden = false
+        stopBtnUI.isHidden = true
+        fullScreenBtn.isHidden = true
+        progressView.isHidden = true
+        leftClock.isHidden = true
+        rightClock.isHidden = true
+        progressTimer.invalidate()
+        progressView.progress = 0.00
+        
+        upTimer.invalidate()
+        upSeconds = 0
+        leftClock.text = "\(timeFormatted(upSeconds))"
+        upTimerIsOn = false
+        
+    }
     
     @objc func silentVideoPause() {
         silentVideoView.player?.pause()
@@ -172,23 +172,23 @@ class VideoCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func shareVideoPressed(_ sender: UIButton) {
-      delegate?.shareVideo(senderCell: self, shareVideoURL: url, silentVideoName: silentVideo)
+        delegate?.shareVideo(senderCell: self, shareVideoURL: url, silentVideoName: silentVideo)
     }
-  
-  
+    
+    
     var buttonAction: ((Any) -> Void)?
-  
+    
     @IBAction func deleteVideoPressed(_ sender: UIButton) {
         delegate?.deleteVideo(senderCell: self, deleteVideoURL: url)
-      
+        
     }
     
     @objc func updateTime() {
         upSeconds = upSeconds + 1
         leftClock.text = "\(timeFormatted(upSeconds))"
-
+        
     }
-
+    
     func timeFormatted(_ totalSeconds: Int) -> String {
         let seconds: Int = totalSeconds % 60
         let minutes: Int = (totalSeconds / 60) % 60
@@ -196,14 +196,12 @@ class VideoCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func fullScreenPressed(_ sender: Any) {
-
+        
         
     }
-  
-    override func setSelected(_ selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
-
-  }
     
-
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
 }
